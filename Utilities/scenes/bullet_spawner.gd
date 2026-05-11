4ace97b188cdc0e2664ef34c6d8e19d66e440e4a
BluePolar <bluepolar282@gmail.com>
2026-05-11 17:36:36 -0500

    Added very basic shooters, bullets, and health systems
    yeuh

--- Utilities/scenes/bullet_spawner.gd
+++ Utilities/scenes/bullet_spawner.gd
@@ -2,6 +2,22 @@ extends Area2D
 
 var bullet = preload("res://Jame-Gam-58/Objects/scenes/bullet.tscn")
 
+
 func _on_timer_timeout() -> void:
-	add_child(bullet.instantiate())
+	spawn_bullet(10)
 	$Timer.start
+
+func spawn_bullet(number):
+	var spawned_bullet = bullet.instantiate()
+	add_child(spawned_bullet)
+	if is_in_group("Left Spawner"):
+		spawned_bullet.add_to_group("Left Spawner")
+		
+	elif is_in_group("Top Spawner"):
+		spawned_bullet.add_to_group("Top Spawner")
+		
+	elif is_in_group("Right Spawner"):
+		spawned_bullet.add_to_group("Right Spawner")
+		
+	elif is_in_group("Bottom Spawner"):
+		spawned_bullet.add_to_group("Bottom Spawner")
